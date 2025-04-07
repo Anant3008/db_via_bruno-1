cffc95788149466de28d8e1c0adb1b5520ce3106
Anant3008 <sastrulaanant@gmail.com>
2025-04-07 10:05:25 +0530

    minor change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const User = require('./models/User');
+const User = require('./user.model');
 
 const app = express();
 app.use(express.json());
